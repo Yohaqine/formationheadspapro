84c4fda8e4efbffb265cbd296f517e88bdeddc91
Yohaqine <spawoda@outlook.fr>
2026-08-06 11:59:32 +0200

    Update LanguageContext.tsx
     //Reviews
        "reviews.label": "Partenaire",
        "reviews.title":"Le meilleur",
        "reviews.title.highlight": "fournisseur"
       "Head Spa Officiel" ( https://headspa-officiel.com/?bg_ref=kgnuknJTzz)

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -104,11 +104,12 @@ const translations = {
     "reviews.gallery.title": "Nos élèves",
     "reviews.gallery.highlight": "certifiés",
 
-   //Reviews
-    "reviews.label": "Partenaire",
-    "reviews.title":"Le meilleur",
-    "reviews.title.highlight": "fournisseur"
-   "Head Spa Officiel" ( https://headspa-officiel.com/?bg_ref=kgnuknJTzz)
+   //Partener
+    "partner.label": "Partenaire",
+    "partner.title":"Le meilleur",
+    "partner.title.highlight": "fournisseur"
+    "partner.name": "Head Spa Officiel" 
+    "partner.url": "https://headspa-officiel.com/?bg_ref=kgnuknJTzz"
 
     // Pricing
     "pricing.label": "Votre Formation",
