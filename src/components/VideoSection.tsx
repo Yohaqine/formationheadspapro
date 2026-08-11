473b297bcc4c4c375cf30ce5861efa6c9402b43b
Yohaqine <spawoda@outlook.fr>
2026-08-11 23:24:54 +0200

    Update VideoSection.tsx

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -50,7 +50,7 @@ const VideoSection = () => {
             />
           </div>
         </motion.div>
-    {/<motion.div
+    <motion.div
   initial={{ opacity: 0, y: 30 }}
   whileInView={{ opacity: 1, y: 0 }}
   viewport={{ once: true }}
